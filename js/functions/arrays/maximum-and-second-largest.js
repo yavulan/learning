3cac99c51d1014fff677e2f0292273150e4ec7d6
yavulan <yavulan@gmail.com>
2016-07-13 16:06:27 +0300

    Add maximum-and-second-largest function

---
+++ js/functions/arrays/maximum-and-second-largest.js
@@ -0,0 +1,43 @@
+var arrs = [
+    [213,1,2,2,34,5,5,6,32,23],
+    [-12,-3,-31,-532,-33],
+    [5,5,5,5,5,5],
+    [5,1,1,1,1,1]
+];
+
+consoleArr();
+
+function maximumAndSecondLargest( array ){
+    var max,
+        secondLargest;
+
+    if ( Array.isArray(array) ){
+        max = secondLargest = -Infinity;
+
+        for( var i = 0; i < array.length; i++ ){
+            if( array[i] > max ){
+                secondLargest = max;
+                max = array [i];
+            } else
+            if ( array[i] < max && array[i] > secondLargest ){
+                secondLargest = array[i];
+            }
+        }
+
+        secondLargest = (secondLargest === -Infinity) ? void 0 : secondLargest;
+    } else {
+        // Not an array
+    }
+
+    return {
+        maximum : max,
+        secondLargest : secondLargest
+    }
+}
+
+function consoleArr(){
+    for( var i = 0; i < arrs.length; i++ ){
+        var res = maximumAndSecondLargest( arrs[i] );
+        console.log( 'Maximum is ' + res.maximum + '; second largest is ' + res.secondLargest );
+    }
+}
