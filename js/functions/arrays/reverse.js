b5513320a419f9ed4a1bef39d99fd00a53e07c47
yavulan <yavulan@gmail.com>
2016-07-13 16:30:21 +0300

    Add reverse array function

---
+++ js/functions/arrays/reverse.js
@@ -0,0 +1,36 @@
+var numbers = [1,2,3,4,5,6,7,8,9];
+var arr = [1,2,3,4,5,6,7,8,9,'String',null];
+
+console.log( reverseNumbersArray(numbers) );
+console.log( reverseAnyArray(arr) );
+
+function reverseNumbersArray( array ){
+    if( Array.isArray(array) ){
+        var len = array.length - 1;
+
+        for( var i = 0; i < len/2; i++ ){
+            array[i] = array[len-i] + (array[len-i] = array[i], 0);
+
+        //    or
+        //    array[i] = array[i] ^ array[len-i];
+        //    array[len-i] = array[i] ^ array[len-i];
+        //    array[i] = array[i] ^ array[len-i];
+        }
+
+        return array;
+    }
+}
+
+function reverseAnyArray( array ){
+    if( Array.isArray(array) ){
+        var len = array.length - 1;
+
+        for( var i = 0; i < len/2; i++ ){
+            var tmp = array[i];
+            array[i] = array[len-i];
+            array[len-i] = tmp;
+        }
+
+        return array;
+    }
+}
