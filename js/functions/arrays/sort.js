d64243e4ac2561f264e962d91db9746e74bfaaa0
yavulan <yavulan@gmail.com>
2016-07-20 14:27:39 +0300

    Add stable sorting

--- js/functions/arrays/sort.js
+++ js/functions/arrays/sort.js
@@ -11,3 +11,11 @@ arr.concat().sort();
 arr.slice().sort();
 // for array-like objects
 Array.prototype.slice.call(arr).sort();
+
+// Stable sorting ( https://en.wikipedia.org/wiki/Sorting_algorithm#Stability )
+// http://blog.vjeux.com/2010/javascript/javascript-sorting-table.html
+
+arr = arr.concat().sort( function (a, b) {
+    return ( a === b ) ? arr.indexOf(a) - arr.indexOf(b) : ( a < b ) ? -1 : 1;
+});
+
