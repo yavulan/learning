4fbcce7c6684bf22c0263c13c651ef6e125022d0
yavulan <yavulan@gmail.com>
2016-07-22 13:56:33 +0300

    Add shorthands.js

---
+++ js/shorthands.js
@@ -0,0 +1,18 @@
+/*
+* If {condition} do {action}
+* */
+if(condition) num++;
+
+// shorthand
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_AND
+condition && num++;
+
+/*
+ * Even or odd
+ * */
+number % 2
+
+// shorthand
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#Bitwise_AND
+number & 1;
+
